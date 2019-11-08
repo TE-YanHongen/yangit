e784798ec1c1b32263beb9362f16532b882f57a9
yanhongen <Hongen.Yan@sony.com>
2019-11-08 11:19:34 +0900

    wrote a test shell file

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Program
+#This program shows "Welcome SONY" in your screen
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo Git is a version control system.
+echo Git is free software.
+
+exit 0
+
