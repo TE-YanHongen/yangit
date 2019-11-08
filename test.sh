efb33b9f761fb46c87a6691022585bff17317879
yanhongen <Hongen.Yan@sony.com>
2019-11-08 13:40:04 +0900

    git tracks changes

--- test.sh
+++ test.sh
@@ -8,5 +8,6 @@ export PATH
 echo Git is a distributed version control system.
 echo Git is free software distributed under GPL.
 echo Git has a mutable index called stage.
+echo Git tracks changes.
 exit 0
 
